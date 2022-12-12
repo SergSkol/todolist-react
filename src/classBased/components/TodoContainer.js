0af52629f3cbbda89a2b37207deeb7fee1841c0e
Serg Skol <sps676@gmail.com>
2022-12-12 14:51:09 +0100

    Prepare for Hooks

--- src/classBased/components/TodoContainer.js
+++ src/classBased/components/TodoContainer.js
@@ -3,31 +3,11 @@ import Header from "./Header";
 import TodosList from "./TodosList";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
-import { saveTasks, loadTasks } from "./LocalStorage";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: loadTasks()
-    // // default values:
-    // [
-      // {
-      //   id: uuidv4(),
-      //   title: "Setup development environment",
-      //   completed: true
-      // },
-      // {
-      //   id: uuidv4(),
-      //   title: "Develop website and add content",
-      //   completed: false
-      // },
-      // {
-      //   id: uuidv4(),
-      //   title: "Deploy to live server",
-      //   completed: false
-      // }
-    // ]
+    todos: [],
    };
-
 
    handleChange = id => {
     this.setState(prevState => ({
@@ -75,8 +55,24 @@ class TodoContainer extends React.Component {
     })
   }
 
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  }
+
   render() {
-    saveTasks(this.state.todos);
     return (
       <div className="container">
         <div className="inner">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './functionBased/index.css';
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./functionBased/components/TodoContainer"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
